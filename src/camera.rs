b99758dd2305661130aed81642ad6d314c7f5f7a
YouSafe <yelagabani@hotmail.com>
2026-09-02 12:10:22 +0200

    Update .gitignore

--- src/camera.rs
+++ src/camera.rs
@@ -15,6 +15,7 @@ impl Camera {
     pub fn pos_screen_to_world(&self, screen: egui::Pos2, screen_center: egui::Pos2) -> egui::Pos2 {
         (Self::vec_flip_y(screen - screen_center) / self.zoom + self.center.to_vec2()).to_pos2()
     }
+    
     pub fn pos_world_to_screen(&self, world: egui::Pos2, screen_center: egui::Pos2) -> egui::Pos2 {
         (Self::vec_flip_y(world - self.center) * self.zoom + screen_center.to_vec2()).to_pos2()
     }
